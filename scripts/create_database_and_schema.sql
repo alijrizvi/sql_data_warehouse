3d736c46783e2d30a6536089a8953bf2a0649cd4
Ali Rizvi <144070197+alijrizvi@users.noreply.github.com>
2026-01-15 15:53:27 -0600

    Add files via upload

---
+++ scripts/create_database_and_schema.sql
@@ -0,0 +1,16 @@
+/*
+
+This script is focused on (Creating a Database and) defining the Schema for the 
+3 Layers - Bronze, Silver, and Gold - of the Database of this project.
+
+Since I did not need to write a script to Create a Database this time
+(i.e., using "CREATE DATABASE IF NOT EXISTS..."), I proceeded straight to coding
+for defining the Schema.
+
+*/
+
+-- Creating for each Planned Layer a Schema
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
